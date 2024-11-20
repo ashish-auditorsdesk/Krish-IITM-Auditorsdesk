2602a5d631654e2d0140c7541eaa49b70cbf0887
krishnamittal <148966133+krishmittal1301@users.noreply.github.com>
2024-11-20 14:58:06 +0530

    Delete BILL_OCR AUDITORS DESK/Bill_ocr directory

--- BILL_OCR AUDITORS DESK/Bill_ocr/postbills/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PostbillsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'postbills'
--- BILL_OCR AUDITORS DESK/Bill_ocr/postbills/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-# process_queued_file()
-
-urlpatterns = [
-    path('hello',views.ocr)
-]
-
